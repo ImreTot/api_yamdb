74bb75267925819c041470749a84d2a07eca485b
Denis Vovk <destiny986ya@yandex.ru>
2023-04-28 23:18:36 +0500

    views, score for titles add

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,11 +3,11 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework import serializers
 
-from reviews.models import Comment, Review, Title, User
+from reviews.models import Comment, Review, Title
 
 class ReviewSerializer(serializers.ModelSerializer):
     """Серилизатор для отзывов."""
-    author = serializers.SlugRelatedField(slug_field='username')
+    author = serializers.SlugRelatedField(slug_field='username', read_only=True)
     score = serializers.IntegerField(required=True)
 
     class Meta:
@@ -20,17 +20,26 @@ class ReviewSerializer(serializers.ModelSerializer):
         author = request.user
         title_id = self.context.get('view').kwargs.get('title_id')
         title = get_object_or_404(Title, id=title_id)
-        if request.method == 'POST':
-            if title.reviews.filter(title=title_id, author=author).exists():
-                raise ValidationError('Вы уже оставляли отзыв!')
+        if request.method == 'POST' and title.reviews.filter(title=title_id, author=author).exists():
+            raise ValidationError('Вы уже оставляли отзыв!')
         return data
 
 
 class CommentSerializer(serializers.ModelSerializer):
     """Серилизатор для комментариев."""
-    author = serializers.SlugRelatedField(slug_field='username')
+    author = serializers.SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
         model = Comment
         fields = '__all__'
-        read_only_fields = ('author', 'review')
+        read_only_fields = ('review', 'author')
+
+
+class TitlePostSerializer(serializers.ModelSerializer):
+    pass
+
+
+class TitleReadSerializer(serializers.ModelSerializer):
+    rating = serializers.IntegerField(
+        source='reviews__score__avg', read_only=True
+    )
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,10 +1,47 @@
+from django.db.models import Avg
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
+from reviews.models import Review, Title
+from .serializers import TitlePostSerializer, TitleReadSerializer, ReviewSerializer, CommentSerializer
+
+
+# https://stackoverflow.com/questions/60602349/average-for-ratings-in-django
+class TitleViewSet(viewsets.ModelViewSet):
+    """Вьюсет для произведений."""
+    queryset = Title.objects.all().annotate(avg_rating=Avg('reviews__score')).order_by('-avg_rating')
+
+    def get_serializer_class(self):
+        if self.request.method in ['POST', 'PUT', 'PATCH']:
+            return TitlePostSerializer
+        return TitleReadSerializer
 
 class ReviewViewSet(viewsets.ModelViewSet):
     """Вьюсет для отзывов."""
-    pass
+    serializer_class = ReviewSerializer
+    # permission_classes = ()
+
+    def get_title(self):
+        id = self.kwargs.get('title_id')
+        return get_object_or_404(Title, id=id)
+
+    def get_queryset(self):
+        return self.get_title().reviews.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, title=self.get_title())
 
 class CommentViewSet(viewsets.ModelViewSet):
     """Вьюсет для комментариев."""
-    pass
+    serializer_class = CommentSerializer
+    # permission_classes = ()
+
+    def get_review(self):
+        id = self.kwargs.get('review_id')
+        return get_object_or_404(Review, id=id)
+
+    def get_queryset(self):
+        return self.get_review().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, review=self.get_review())
